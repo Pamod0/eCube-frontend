ec152d74095dd1f08fa579fa5792073b361c6c04
Pamod <pamodchandulapeiris@gmail.com>
2025-02-19 14:00:43 +0530

    email intergration done

--- src/app/layout/contact-form/contact-form.component.ts
+++ src/app/layout/contact-form/contact-form.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormControl, FormGroupDirective, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormGroupDirective, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { EmailService } from '../../services/email.service';
+import { HttpClientModule } from '@angular/common/http';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -17,11 +19,55 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 
 @Component({
     selector: 'app-contact-form',
-    imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule],
+    imports: [
+        ReactiveFormsModule,
+        HttpClientModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatIconModule
+    ],
     templateUrl: './contact-form.component.html',
     styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
+    contactForm = new FormGroup({
+        name: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        phone: new FormControl(''),
+        message: new FormControl('', [Validators.required])
+    });
+
+    isSubmitting = false;
+    successMessage: string | null = null;
+    errorMessage: string | null = null;
+
+    constructor(private emailService: EmailService) {}
+
+    submitForm() {
+        if (this.contactForm.invalid) {
+            return;
+        }
+
+        this.isSubmitting = true;
+        this.successMessage = null;
+        this.errorMessage = null;
+
+        this.emailService.sendEmail(this.contactForm.value).subscribe({
+            next: () => {
+                this.successMessage = 'Your message has been sent!';
+                this.contactForm.reset();
+            },
+            error: (err) => {
+                this.errorMessage = 'Failed to send message. Please try again.';
+                console.error(err);
+            },
+            complete: () => {
+                this.isSubmitting = false;
+            }
+        });
+    }
+
     emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
     matcher = new MyErrorStateMatcher();
