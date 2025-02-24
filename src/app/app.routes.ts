08fb76ed92150680223a30ab3bb558cbd5531bc0
Sarujan <Sarujan123>
2025-02-24 09:34:57 +0530

    Blogs addes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TestComponent } from './layout/test/test.component';
 import { CareersComponent } from './pages/careers/careers.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,10 @@ export const routes: Routes = [
     {
         path: 'careers',
         component: CareersComponent
+    },
+    {
+        path: 'blog',
+        component: BlogComponent
     },
     {
         path: 'test',
