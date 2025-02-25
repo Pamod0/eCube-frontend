ed4dcacd0717c299c6b9e2d5f7776cf972a12650
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2025-02-25 23:27:47 +0530

    routerlinks added

--- src/app/layout/our-projects/our-projects.component.ts
+++ src/app/layout/our-projects/our-projects.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-our-projects',
-  imports: [MatIconModule],
-  templateUrl: './our-projects.component.html',
-  styleUrl: './our-projects.component.scss'
+    selector: 'app-our-projects',
+    imports: [RouterLink, MatIconModule],
+    templateUrl: './our-projects.component.html',
+    styleUrl: './our-projects.component.scss'
 })
 export class OurProjectsComponent {
-
+    scrollToTop() {
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // Smooth scrolling to the top
+    }
 }
