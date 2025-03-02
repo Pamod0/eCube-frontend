606b8e616cfb1ee2d7e908d28a7285e107023c8d
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2025-03-02 15:11:13 +0530

    project list page

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Project {
+    name: string;
+    logo: string;
+    image: string;
+}
+
 @Component({
     selector: 'app-projects',
     imports: [],
@@ -7,5 +13,40 @@ import { Component } from '@angular/core';
     styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-    projects: any;
+    projects: Project[];
+
+    ngOnInit() {
+        this.projects = [
+            {
+                name: 'Sri Lanka Cricket <br> Vendor Management System',
+                logo: '/assets/images/projects/logo-slc.png',
+                image: '/assets/images/projects/oceanwatch.png'
+            },
+            {
+                name: 'Sri Lanka Cricket <br> Vendor Management System',
+                logo: '/assets/images/projects/logo-slc.png',
+                image: '/assets/images/projects/oceanwatch.png'
+            },
+            {
+                name: 'Sri Lanka Cricket <br> Vendor Management System',
+                logo: '/assets/images/projects/logo-slc.png',
+                image: '/assets/images/projects/oceanwatch.png'
+            },
+            {
+                name: 'Sri Lanka Cricket <br> Vendor Management System',
+                logo: '/assets/images/projects/logo-slc.png',
+                image: '/assets/images/projects/oceanwatch.png'
+            },
+            {
+                name: 'Sri Lanka Cricket <br> Vendor Management System',
+                logo: '/assets/images/projects/logo-slc.png',
+                image: '/assets/images/projects/oceanwatch.png'
+            },
+            {
+                name: 'Sri Lanka Cricket <br> Vendor Management System',
+                logo: '/assets/images/projects/logo-slc.png',
+                image: '/assets/images/projects/oceanwatch.png'
+            },
+        ];
+    }
 }
