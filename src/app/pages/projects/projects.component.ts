698d18b22e193f03e885bff4421d9f1eff0998ed
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2025-03-03 10:16:43 +0530

    projects list page

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 interface Project {
     name: string;
     logo: string;
     image: string;
+    link?: string;
 }
 
 @Component({
     selector: 'app-projects',
-    imports: [],
+    imports: [RouterLink],
     templateUrl: './projects.component.html',
     styleUrl: './projects.component.scss'
 })
@@ -20,32 +22,38 @@ export class ProjectsComponent {
             {
                 name: 'Sri Lanka Cricket <br> Vendor Management System',
                 logo: '/assets/images/projects/logo-slc.png',
-                image: '/assets/images/projects/oceanwatch.png'
+                image: '/assets/images/projects/oceanwatch.png',
+                link: '/SLC'
+            },
+            {
+                name: 'OceanWatch | MEPA - Beach Cleanup <br> Booking Management System',
+                logo: '/assets/images/projects/logo-mepa.png',
+                image: '/assets/images/projects/oceanwatch.png',
+                link: '/SLC'
+            },
+            {
+                name: 'Basketball Sri Lanka Website & Players Management System',
+                logo: '/assets/images/projects/logo-slbf.png',
+                image: '/assets/images/projects/oceanwatch.png',
+                link: '/SLC'
             },
             {
                 name: 'Sri Lanka Cricket <br> Vendor Management System',
                 logo: '/assets/images/projects/logo-slc.png',
-                image: '/assets/images/projects/oceanwatch.png'
+                image: '/assets/images/projects/oceanwatch.png',
+                link: '/SLC'
             },
             {
                 name: 'Sri Lanka Cricket <br> Vendor Management System',
                 logo: '/assets/images/projects/logo-slc.png',
-                image: '/assets/images/projects/oceanwatch.png'
+                image: '/assets/images/projects/oceanwatch.png',
+                link: '/SLC'
             },
             {
                 name: 'Sri Lanka Cricket <br> Vendor Management System',
                 logo: '/assets/images/projects/logo-slc.png',
-                image: '/assets/images/projects/oceanwatch.png'
-            },
-            {
-                name: 'Sri Lanka Cricket <br> Vendor Management System',
-                logo: '/assets/images/projects/logo-slc.png',
-                image: '/assets/images/projects/oceanwatch.png'
-            },
-            {
-                name: 'Sri Lanka Cricket <br> Vendor Management System',
-                logo: '/assets/images/projects/logo-slc.png',
-                image: '/assets/images/projects/oceanwatch.png'
+                image: '/assets/images/projects/oceanwatch.png',
+                link: '/SLC'
             },
         ];
     }
