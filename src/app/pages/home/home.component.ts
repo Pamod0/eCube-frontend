cadd54132aeb8c2eb4367a738d503c9fbc0a5016
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2025-03-06 12:56:39 +0530

    refactor

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,7 @@ import { HeroComponent } from './hero/hero.component';
 import { LogoSliderComponent } from './logo-slider/logo-slider.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { OurServicesComponent } from './our-services/our-services.component';
-import { OurProjectsComponent } from '../our-projects/our-projects.component';
+import { OurProjectsComponent } from './our-projects/our-projects.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 
 @Component({
