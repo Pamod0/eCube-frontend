5fb53cc499f21a572975ecf7c32927ee6ba5eda2
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2025-03-06 12:25:58 +0530

    refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { TestComponent } from './layout/test/test.component';
 import { CareersComponent } from './pages/careers/careers.component';
 import { BlogComponent } from './pages/blog/blog.component';
@@ -9,12 +9,12 @@ import { WebDesignServiceComponent } from './pages/service-inner/web-design-serv
 import { BlogPageComponent } from './pages/blog/blog-page/blog-page/blog-page.component';
 import { UiUxDesignServiceComponent } from './pages/service-inner/ui-ux-design-service/ui-ux-design-service.component';
 import { BrandIdentityServiceComponent } from './pages/service-inner/brand-identity-service/brand-identity-service.component';
-import { SrilankaCricketComponent } from './layout/our-projects/srilanka-cricket/srilanka-cricket.component';
-import { MepaComponent } from './layout/our-projects/mepa/mepa.component';
-import { SLBasketballComponent } from './layout/our-projects/sl-basketball/sl-basketball.component';
-import { NOHSCOPComponent } from './layout/our-projects/nohscop/nohscop.component';
-import { CycloneRCMComponent } from './layout/our-projects/cyclone-rcm/cyclone-rcm.component';
-import { TourpillComponent } from './layout/our-projects/tourpill/tourpill.component';
+// import { SrilankaCricketComponent } from './layout/our-projects/srilanka-cricket/srilanka-cricket.component';
+// import { MepaComponent } from './layout/our-projects/mepa/mepa.component';
+// import { SLBasketballComponent } from './layout/our-projects/sl-basketball/sl-basketball.component';
+// import { NOHSCOPComponent } from './layout/our-projects/nohscop/nohscop.component';
+// import { CycloneRCMComponent } from './layout/our-projects/cyclone-rcm/cyclone-rcm.component';
+// import { TourpillComponent } from './layout/our-projects/tourpill/tourpill.component';
 import { TestPageComponent } from './pages/test-page/test-page.component';
 import { ProjectInnerComponent } from './pages/project-inner/project-inner.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
@@ -80,30 +80,30 @@ export const routes: Routes = [
         path: 'blog-page',
         component: BlogPageComponent
     },
-    {
-        path: 'SLC',
-        component: SrilankaCricketComponent
-    },
-    {
-        path: 'mepa',
-        component: MepaComponent
-    },
-    {
-        path: 'sl-basketball',
-        component: SLBasketballComponent
-    },
-    {
-        path: 'nohscop',
-        component: NOHSCOPComponent
-    },
-    {
-        path: 'cyclone',
-        component: CycloneRCMComponent
-    },
-    {
-        path: 'tourpill',
-        component: TourpillComponent
-    },
+    // {
+    //     path: 'SLC',
+    //     component: SrilankaCricketComponent
+    // },
+    // {
+    //     path: 'mepa',
+    //     component: MepaComponent
+    // },
+    // {
+    //     path: 'sl-basketball',
+    //     component: SLBasketballComponent
+    // },
+    // {
+    //     path: 'nohscop',
+    //     component: NOHSCOPComponent
+    // },
+    // {
+    //     path: 'cyclone',
+    //     component: CycloneRCMComponent
+    // },
+    // {
+    //     path: 'tourpill',
+    //     component: TourpillComponent
+    // },
     {
         path: 'services/:service',
         component: TestPageComponent
--- src/app/pages/blog/blog-page/blog-page/blog-page.component.ts
+++ src/app/pages/blog/blog-page/blog-page/blog-page.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../../../layout/header/header.component";
+import { HeaderComponent } from '../../../../layout/header/header.component';
 import { MatIconModule } from '@angular/material/icon';
-import { AccordionComponent } from "../../../../layout/accordion/accordion.component";
-import { ContactFormComponent } from "../../../../layout/contact-form/contact-form.component";
-import { FooterComponent } from "../../../../layout/footer/footer.component";
+import { AccordionComponent } from '../../../../layout/accordion/accordion.component';
+import { ContactFormComponent } from '../../../../layout/contact-form/contact-form.component';
+import { FooterComponent } from '../../../../layout/footer/footer.component';
 
 @Component({
-  selector: 'app-blog-page',
-  imports: [HeaderComponent, MatIconModule, AccordionComponent, ContactFormComponent, FooterComponent],
-  templateUrl: './blog-page.component.html',
-  styleUrl: './blog-page.component.scss'
+    selector: 'app-blog-page',
+    imports: [HeaderComponent, MatIconModule, AccordionComponent, ContactFormComponent, FooterComponent],
+    templateUrl: './blog-page.component.html',
+    styleUrl: './blog-page.component.scss'
 })
-export class BlogPageComponent {
-
-}
+export class BlogPageComponent {}
