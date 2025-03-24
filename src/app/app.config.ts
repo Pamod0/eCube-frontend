5d43b4b943017e9188e647d7da2005b6cd544c63
Pamod <pamodchandulapeiris@gmail.com>
2025-03-24 14:30:36 +0530

    fragment scrolling and usehash disble

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -13,7 +13,15 @@ import Material from '@primeng/themes/material';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation()),
+    provideRouter(
+      routes,
+      // withHashLocation(),
+      withViewTransitions(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      }),
+    ),
     provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG({
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, ElementRef, inject, Inject, ViewChild } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule, ViewportScroller } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
@@ -14,11 +14,14 @@ export class HeaderComponent {
   faCaretDown = faCaretDown;
   isDropdownOpen = false;
 
+  private router = inject(Router);
+
   scrollToSection(elementId: string): void {
     const element = document.getElementById(elementId);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
+    // if (element) {
+    //   element.scrollIntoView({ behavior: 'smooth' });
+    // }
+    this.router.navigate(['/'], { fragment: elementId });
   }
   /* Open when someone clicks on the span element */
   openNav() {
