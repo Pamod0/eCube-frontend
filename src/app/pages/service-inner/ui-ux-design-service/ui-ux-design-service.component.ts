83982ab51112f987d8f3c62fb59d5b1df4b384c1
Pamod <pamodchandulapeiris@gmail.com>
2025-03-25 14:01:45 +0530

    content update

--- src/app/pages/service-inner/ui-ux-design-service/ui-ux-design-service.component.ts
+++ src/app/pages/service-inner/ui-ux-design-service/ui-ux-design-service.component.ts
@@ -3,75 +3,75 @@ import { Carousel } from '../carousel.model';
 import { ServiceInnerComponent } from '../service-inner.component';
 
 @Component({
-    selector: 'app-ui-ux-design-service',
-    imports: [ServiceInnerComponent],
-    templateUrl: './ui-ux-design-service.component.html',
-    styleUrl: './ui-ux-design-service.component.scss'
+  selector: 'app-ui-ux-design-service',
+  imports: [ServiceInnerComponent],
+  templateUrl: './ui-ux-design-service.component.html',
+  styleUrl: './ui-ux-design-service.component.scss',
 })
 export class UiUxDesignServiceComponent {
-    serviceName: string = 'Ui/UX Design';
+  serviceName: string = 'Ui/UX Design';
 
-    mainTitleOne: string = `Seamless, Intuitive, and<br />
+  mainTitleOne: string = `Seamless, Intuitive, and<br />
                     <span class="poppins-medium italic">AI-Enhanced</span> UI/UX Design`;
 
-    imageUrl: string = '/assets/images/services/ui-ux-design/service.png';
+  imageUrl: string = '/assets/images/services/ui-ux-design/service.png';
 
-    paragraphOne: string = `UI/UX design is the foundation of every successful digital product, shaping how users interact with websites, apps, and software. 
-                User Interface (UI) design focuses on the look and feel—creating visually appealing layouts, buttons, and elements that make navigation seamless. 
-                User Experience (UX) design, on the other hand, ensures that every interaction is intuitive, efficient, and enjoyable. Together, UI and UX define 
-                how a product functions and feels, directly impacting user satisfaction, engagement, and conversion rates.`;
+  paragraphOne: string = `UI/UX design is the foundation of every successful digital product, shaping how users interact with websites, apps, and software. 
+                          User Interface (UI) design focuses on the look and feel—creating visually appealing layouts, buttons, and elements that make navigation 
+                          seamless. User Experience (UX) design, on the other hand, ensures that every interaction is intuitive, efficient, and enjoyable. 
+                          Together, UI and UX define how a product functions and feels, directly impacting user satisfaction, engagement, and conversion rates.`;
 
-    paragraphTwo: string = `At ecube, we go beyond aesthetics—we craft AI-enhanced, user-centric designs that optimize usability and elevate brand identity. 
-                Our process is rooted in data-driven insights, allowing us to create intuitive workflows, responsive layouts, and frictionless experiences that 
-                keep users engaged. By leveraging AI-powered design tools and predictive analytics, we accelerate the design process while ensuring every detail 
-                enhances usability. Whether it’s a website, mobile app, or enterprise platform, we design with purpose—helping businesses deliver digital experiences 
-                that truly connect with their audience.`;
+  paragraphTwo: string = `At Ecube, we go beyond aesthetics—we craft AI-enhanced, user-centric designs that optimize usability and elevate brand identity. 
+                          Our process is rooted in data-driven insights, allowing us to create intuitive workflows, responsive layouts, and frictionless experiences 
+                          that keep users engaged. By leveraging AI-powered design tools and predictive analytics, we accelerate the design process while ensuring 
+                          every detail enhances usability. Whether it’s a website, mobile app, or enterprise platform, we design with purpose—helping businesses 
+                          deliver digital experiences that truly connect with their audience.`;
 
-    carouselHeader: string = 'Industry-Leading Tools for Next-Level UI/UX';
+  carouselHeader: string = 'Industry-Leading Tools for Next-Level UI/UX';
 
-    carouselItems: Carousel[] = [
-        {
-            id: 1,
-            image: '/assets/images/services/ui-ux-design/carousel-1.png'
-        },
-        {
-            id: 2,
-            image: '/assets/images/services/ui-ux-design/carousel-2.png'
-        },
-        {
-            id: 3,
-            image: '/assets/images/services/ui-ux-design/carousel-3.png'
-        },
-        {
-            id: 4,
-            image: '/assets/images/services/ui-ux-design/carousel-4.png'
-        },
-        {
-            id: 5,
-            image: '/assets/images/services/ui-ux-design/carousel-5.png'
-        },
-        {
-            id: 6,
-            image: '/assets/images/services/ui-ux-design/carousel-6.png'
-        },
-        {
-            id: 7,
-            image: '/assets/images/services/ui-ux-design/carousel-7.png'
-        },
-        {
-            id: 8,
-            image: '/assets/images/services/ui-ux-design/carousel-8.png'
-        },
-        {
-            id: 9,
-            image: '/assets/images/services/ui-ux-design/carousel-9.png'
-        },
-        {
-            id: 10,
-            image: '/assets/images/services/ui-ux-design/carousel-10.png'
-        }
-    ];
+  carouselItems: Carousel[] = [
+    {
+      id: 1,
+      image: '/assets/images/services/ui-ux-design/carousel-1.png',
+    },
+    {
+      id: 2,
+      image: '/assets/images/services/ui-ux-design/carousel-2.png',
+    },
+    {
+      id: 3,
+      image: '/assets/images/services/ui-ux-design/carousel-3.png',
+    },
+    {
+      id: 4,
+      image: '/assets/images/services/ui-ux-design/carousel-4.png',
+    },
+    {
+      id: 5,
+      image: '/assets/images/services/ui-ux-design/carousel-5.png',
+    },
+    {
+      id: 6,
+      image: '/assets/images/services/ui-ux-design/carousel-6.png',
+    },
+    {
+      id: 7,
+      image: '/assets/images/services/ui-ux-design/carousel-7.png',
+    },
+    {
+      id: 8,
+      image: '/assets/images/services/ui-ux-design/carousel-8.png',
+    },
+    {
+      id: 9,
+      image: '/assets/images/services/ui-ux-design/carousel-9.png',
+    },
+    {
+      id: 10,
+      image: '/assets/images/services/ui-ux-design/carousel-10.png',
+    },
+  ];
 
-    mainTitleTwo: string = `Transformed Ideas into Beautiful, <br />
+  mainTitleTwo: string = `Transformed Ideas into Beautiful, <br />
                 <span class="poppins-medium italic">Functional Designs</span>`;
 }
--- src/app/pages/service-inner/web-design-service/web-design-service.component.ts
+++ src/app/pages/service-inner/web-design-service/web-design-service.component.ts
@@ -3,75 +3,75 @@ import { ServiceInnerComponent } from '../service-inner.component';
 import { Carousel } from '../carousel.model';
 
 @Component({
-    selector: 'app-web-design-service',
-    imports: [ServiceInnerComponent],
-    templateUrl: './web-design-service.component.html',
-    styleUrl: './web-design-service.component.scss'
+  selector: 'app-web-design-service',
+  imports: [ServiceInnerComponent],
+  templateUrl: './web-design-service.component.html',
+  styleUrl: './web-design-service.component.scss',
 })
 export class WebDesignServiceComponent {
-    serviceName: string = 'Web Design';
+  serviceName: string = 'Web Design';
 
-    mainTitleOne: string = `Crafting Stunning, <span class="poppins-medium italic"> High-Performance</span> <br />
+  mainTitleOne: string = `Crafting Stunning, <span class="poppins-medium italic"> High-Performance</span> <br />
                     Websites with <span class="poppins-medium italic">AI Precision</span>`;
 
-    imageUrl: string = '/assets/images/services/web-design/service.png';
+  imageUrl: string = '/assets/images/services/web-design/service.png';
 
-    paragraphOne: string = `Web design is the art and science of creating visually appealing, user-friendly, and high-performing websites that leave a lasting 
-                            impression. A great website is more than just colors and layouts—it’s a strategic digital experience that guides visitors, enhances 
-                            engagement, and drives conversions. Every element, from typography to navigation, plays a role in how users perceive and interact with 
-                            your brand. A well-crafted website builds credibility, strengthens brand identity, and ensures a seamless journey across all devices.`;
+  paragraphOne: string = `Web design is the art and science of creating visually appealing, user-friendly, and high-performing websites that leave a lasting impression. 
+                          A great website is more than just colors and layouts—it’s a strategic digital experience that guides visitors, enhances engagement, and drives 
+                          conversions. Every element, from typography to navigation, plays a role in how users perceive and interact with your brand. 
+                          A well-crafted website builds credibility, strengthens brand identity, and ensures a seamless journey across all devices.`;
 
-    paragraphTwo: string = `At ecube, we take web design to the next level with an AI-powered, results-driven approach. By combining creativity with advanced 
-                            technologies, we create websites that are not only aesthetically stunning but also optimized for speed, performance, and user experience. 
-                            Our AI-driven processes accelerate development while maintaining precision, ensuring that your website is launched faster without 
-                            compromising quality. Whether you're a startup looking to make an impact or an enterprise aiming for digital transformation, we craft custom 
-                            solutions that align with your business goals and industry standards.`;
+  paragraphTwo: string = `At ecube, we take web design to the next level with an AI-powered, results-driven approach. By combining creativity with advanced technologies, 
+                          we create websites that are not only aesthetically stunning but also optimized for speed, performance, and user experience. Our AI-driven processes 
+                          accelerate development while maintaining precision, ensuring that your website is launched faster without compromising quality. 
+                          Whether you're a startup looking to make an impact or an enterprise aiming for digital transformation, we craft custom solutions 
+                          that align with your business goals and industry standards.`;
 
-    carouselHeader: string = 'Innovative Tech Stack for Modern Websites';
+  carouselHeader: string = 'Innovative Tech Stack for Modern Websites';
 
-    carouselItems: Carousel[] = [
-        {
-            id: 1,
-            image: '/assets/images/services/web-design/carousel-1.png'
-        },
-        {
-            id: 2,
-            image: '/assets/images/services/web-design/carousel-2.png'
-        },
-        {
-            id: 3,
-            image: '/assets/images/services/web-design/carousel-3.png'
-        },
-        {
-            id: 4,
-            image: '/assets/images/services/web-design/carousel-4.png'
-        },
-        {
-            id: 5,
-            image: '/assets/images/services/web-design/carousel-5.png'
-        },
-        {
-            id: 6,
-            image: '/assets/images/services/web-design/carousel-6.png'
-        },
-        {
-            id: 7,
-            image: '/assets/images/services/web-design/carousel-7.png'
-        },
-        {
-            id: 8,
-            image: '/assets/images/services/web-design/carousel-8.png'
-        },
-        {
-            id: 9,
-            image: '/assets/images/services/web-design/carousel-9.png'
-        },
-        {
-            id: 10,
-            image: '/assets/images/services/web-design/carousel-10.png'
-        }
-    ];
+  carouselItems: Carousel[] = [
+    {
+      id: 1,
+      image: '/assets/images/services/web-design/carousel-1.png',
+    },
+    {
+      id: 2,
+      image: '/assets/images/services/web-design/carousel-2.png',
+    },
+    {
+      id: 3,
+      image: '/assets/images/services/web-design/carousel-3.png',
+    },
+    {
+      id: 4,
+      image: '/assets/images/services/web-design/carousel-4.png',
+    },
+    {
+      id: 5,
+      image: '/assets/images/services/web-design/carousel-5.png',
+    },
+    {
+      id: 6,
+      image: '/assets/images/services/web-design/carousel-6.png',
+    },
+    {
+      id: 7,
+      image: '/assets/images/services/web-design/carousel-7.png',
+    },
+    {
+      id: 8,
+      image: '/assets/images/services/web-design/carousel-8.png',
+    },
+    {
+      id: 9,
+      image: '/assets/images/services/web-design/carousel-9.png',
+    },
+    {
+      id: 10,
+      image: '/assets/images/services/web-design/carousel-10.png',
+    },
+  ];
 
-    mainTitleTwo: string = `Empowered Businesses <br />
+  mainTitleTwo: string = `Empowered Businesses <br />
               <span class="poppins-medium italic">Across Industries</span>`;
 }
